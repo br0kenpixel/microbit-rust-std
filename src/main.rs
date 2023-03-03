5cc2f9f86b7efd9a6afa8e72be9d51b62a90a7a8
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2023-03-03 12:03:49 +0100

    fix broken template - replace `cortex-m-semihosting` with `rtt-target` - cleanup `Embed.toml`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 #![no_main]
 #![feature(restricted_std)]
 
-use std::thread::sleep;
-use std::time::Duration;
 use cortex_m_rt::entry;
-use microbit::board::Board;
-use cortex_m_semihosting::hprintln;
+use microbit::{board::Board, hal::prelude::*, hal::Timer};
+use rtt_target::{rprintln, rtt_init_print};
 
 use alloc_cortex_m::CortexMHeap;
 #[global_allocator]
@@ -14,15 +12,19 @@ const HEAP_SIZE: usize = 1024; // in bytes
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
     unsafe { ALLOCATOR.init(cortex_m_rt::heap_start() as usize, HEAP_SIZE) }
+
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+
     let _nums = vec![1, 2, 3];
     let _text = String::from("Hello World!");
-    let _board = Board::take().unwrap();
 
     loop {
-        hprintln!("Hello!");
-        hprintln!("Vector: {_nums}");
-        hprintln!("String: {_text}");
-        sleep(Duration::from_secs(3));
+        rprintln!("Hello!");
+        rprintln!("Vector: {:?}", _nums);
+        rprintln!("String: {}", _text);
+        timer.delay_ms(1000u16);
     }
 }
